7454877e2873625cad54f36a63a03147111d89d9
prashantkumar999 <prashant047alg@gmail.com>
2026-01-08 12:40:56 +0530

    pagination page added

---
+++ src/components/Home.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import axios from "axios"
+const Home = () => {
+    interface Product {
+        id: number,
+        title: string,
+        description: string,
+        price: number,
+        thumbnail: string,
+    }
+    const pageSize = 5;
+    const [products, setProducts] = useState<Product[]>([])
+    const [currentPage, setCurrentPage] = useState<number>(3)
+    const [loading, setLoading] = useState<boolean>(false)
+    const start = (currentPage - 1) * pageSize;
+    const end = start + pageSize;
+
+    let totalPage = Math.ceil(products.length / 5);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true)
+                const response = await axios.get('https://dummyjson.com/products')
+                setProducts(response.data.products)
+                setLoading(false)
+            } catch (err: unknown) {
+                if (err instanceof Error) {
+                    console.log(err.message)
+                }
+            }
+        }
+        fetchData();
+    }, [])
+
+    const onPreviousHandler = () => {
+        if (currentPage === 1) {
+            return;
+        }
+        setCurrentPage(prev => prev - 1)
+
+    }
+    const onNextHandler = () => {
+        if (currentPage === totalPage) return;
+        setCurrentPage(prev => prev + 1)
+    }
+    console.log(products)
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <h2 className=''>Products</h2>
+            {loading && <h1>loading....</h1>}
+            <div className='flex flex-row flex-wrap w-[80%] mx-auto'>
+                {
+                    products.slice(start, end).map(product => <div className='w-60 h-120 border flex flex-col justify-center items-center m-auto p-2' key={product.id}>
+                        <p className=''>{product.title}</p>
+                        <img src={product.thumbnail} />
+                        <p>{product.description}</p>
+                        <p>{product.price}</p>
+                    </div>)
+                }
+            </div>
+            <div className='flex gap-4 pt-4'>
+                <button className='w-30 px-4 py-2 border' onClick={onPreviousHandler}>previous</button>
+                {
+                    Array.from({ length: totalPage }, (_, index) => <div onClick={() => setCurrentPage(index + 1)} className={`px-4 py-2 border ${currentPage == index + 1 ? "bg-green-200" : ""}`}>
+                        <p>{index + 1}</p>
+                    </div>)
+                }
+                <button className='w-30 px-4 py-2 border' onClick={onNextHandler}>next</button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
