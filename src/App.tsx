eb41ba93057e442360ca543482023e516099b3a4
prashantkumar999 <prashant047alg@gmail.com>
2026-01-08 15:30:33 +0530

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes,Link, Route } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
-
 function App() {
   return (
    <div className=''>
-    <Home/>
+    <BrowserRouter>
+    <nav className='border py-4 flex gap-10 justify-between px-20'>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/cart"}>Cart</Link>
+      {/* <Link to={"/"}>Home</Link> */}
+    </nav>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
    </div>
   )
 }
-
 export default App
